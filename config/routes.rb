9f278fa633da2bcba2b7ace666dafbf162670b4e
Jonah Potter <jonah.potter@protrainings.com>
2014-02-06 16:57:57 -0500

    more/better bootstrap, removed obselete classes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Wiki::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :to => 'homepage#index'
+  root :to => 'nodes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
